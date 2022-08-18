a3731045d48c74a2cf36a299ef65288228a14a29
nitin4679 <nitinjakhar1527@gmail.com>
2022-08-18 19:18:03 -0400

    Models created and established 1 to 1 relationship

---
+++ ASP_FinalExam_Net6/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ASP_FinalExam_Net6.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        [Required] // this is to make it a required field
+        [MaxLength(100, ErrorMessage = "Name cannot be greater than 100 characters!!!")] // when max-length is greater than 100, it will display this message
+        public string Name { get; set; }
+
+        [Range(0,Int32.MaxValue)] // this is to set the minimum value to 0
+        public int EmployeeCount { get; set; }
+
+        [ForeignKey("Employee")]
+        public int? EmployeeId { get; set; }
+        public Employee? Employee { get; set; }
+    }
+}
---
+++ ASP_FinalExam_Net6/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace ASP_FinalExam_Net6.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required] // this is to make it a required field
+        [MaxLength(100, ErrorMessage = "Name cannot be greater than 100 characters!!!")] // when max-length is greater than 100, it will display this message
+        public string Name { get; set; }
+        public bool IsManager { get; set; }
+
+    }
+}
