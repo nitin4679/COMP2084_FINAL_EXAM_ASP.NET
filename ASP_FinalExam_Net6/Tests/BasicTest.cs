65d8b294f7f62169c549e3bb3c5051bffe1661e0
nitin4679 <nitinjakhar1527@gmail.com>
2022-08-18 20:32:17 -0400

    Made test class to test controller method

---
+++ ASP_FinalExam_Net6/Tests/BasicTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ASP_FinalExam_Net6.Data;
+using ASP_FinalExam_Net6.Models;
+
+namespace ASP_FinalExam_Net6.Tests
+{
+    public class BasicTest
+    {
+        protected readonly DbContextOptions<ApplicationDbContext> _opts;
+
+        public BasicTest()
+        {
+            _opts = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "ASP_FinalExam_Net6").Options;
+        }
+
+        protected List<Employee> MakeFakeEmployees(int i)
+        {
+            var employees = new List<Employee>();
+
+            for (int j = 0; j < i; j++)
+            {
+                employees.Add(new Employee
+                {
+                    Id = j + 1,
+                    Name = $"testTable{j + 2}",
+                    IsManager = true,
+                });
+            }
+            return employees;
+        }
+    }
+}
---
+++ ASP_FinalExam_Net6/Tests/EmployeeControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ASP_FinalExam_Net6.Controllers;
+using ASP_FinalExam_Net6.Data;
+using ASP_FinalExam_Net6.Models;
+
+
+namespace ASP_FinalExam_Net6.Tests
+{
+    public class EmployeeControllerTest : BasicTest
+    {
+        [Fact]
+        public async Task Index_Returns_ViewResult_And_EmployeeList()
+        {
+            using(var testDb = new ApplicationDbContext(this._opts))
+            {
+                var testCtrl = new EmployeesController(testDb);
+                var result = await testCtrl.Index();
+                var resVr = Assert.IsType<ViewResult>(result);
+                Assert.IsAssignableFrom<IEnumerable<Employee>>(resVr.ViewData.Model);
+            }
+        }
+        [Fact]
+        public async Task Add_and_Remove()
+        {
+            using (var testDb = new ApplicationDbContext(this._opts))
+            {
+                var testCtrl = new EmployeesController(testDb);
+                var fakeEmployees = MakeFakeEmployees(3);
+
+            }
+        }
+
+
+    }
+}
